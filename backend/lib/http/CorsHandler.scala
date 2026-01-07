fd3f3b013309dae224de01b336f8540752b2afa7
yoshi1012 <carrotpaste1012@gmail.com>
2026-01-07 11:57:32 -0800

    fix: added cors allow template

--- backend/lib/http/CorsHandler.scala
+++ backend/lib/http/CorsHandler.scala
@@ -1,1 +1,15 @@
 package com.taskmanagement.lib.http.demo1
+
+import org.apache.pekko.http.scaladsl.model.HttpMethods._
+import org.apache.pekko.http.scaladsl.model.headers._
+import org.apache.pekko.http.scaladsl.model.{HttpResponse, StatusCodes}
+import org.apache.pekko.http.scaladsl.server.Directives._
+import org.apache.pekko.http.scaladsl.server.{Directive0, Route}
+
+object CorsHander{
+
+    private val allowedOrigins: Set[String] = Set(
+        
+    )
+
+}
