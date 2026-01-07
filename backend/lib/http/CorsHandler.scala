5290ca642ed2c2e6de29e00b71d803273b6ecee9
yoshi1012 <carrotpaste1012@gmail.com>
2026-01-07 13:28:32 -0800

    chore: renamed files based on my coding rules

--- backend/lib/http/CorsHandler.scala
+++ backend/lib/http/CorsHandler.scala
@@ -1,15 +1,44 @@
 package com.taskmanagement.lib.http.demo1
 
-import org.apache.pekko.http.scaladsl.model.HttpMethods._
+import com.typesafe.config.ConfigFactory
+import org.apache.pekko.http.scaladsl.model.{HttpMethods => HM}._
 import org.apache.pekko.http.scaladsl.model.headers._
-import org.apache.pekko.http.scaladsl.model.{HttpResponse, StatusCodes}
-import org.apache.pekko.http.scaladsl.server.Directives._
-import org.apache.pekko.http.scaladsl.server.{Directive0, Route}
+import org.apache.pekko.http.scaladsl.model.{HttpResponse => HR, StatusCodes => SC}
+import org.apache.pekko.http.scaladsl.server.{Directives => D}._
+import org.apache.pekko.http.scaladsl.server.{Directive0 => D0, Route => R}
+import scala.jdk.CollectionConverters._
 
-object CorsHander{
+object CorsHandler{
 
-    private val allowedOrigins: Set[String] = Set(
-        
-    )
+    private val config = ConfigFactory.load()
 
+    private val allowedOrigins: Set[String] = 
+        config.getStringList("cors.allowed-origins").asScala.toSet
+
+    private val allowedMethods: Seq[HM.HttpMethod] = Seq(GET, POST, PUT, DELETE, OPTIONS)
+
+    private val allowedHeaders: Seq[String] = Seq("Authorization", "Content-Type")
+
+    private def corsResponseHeaders(originUrl: String): Directive0 = {
+        D.respondWithHeaders(
+            `Access-Control-Allow-Origin`(HttpOrigin(originUrl)),
+            `Access-Control-Allow-Methods`(allowedMethods),
+            `Access-Control-Allow-Headers`(allowedHeaders),
+            `Access-Control-Allow-Credentials`(true)
+        )
+    }
+
+    def withCors(apiRoute: Route): Route = {
+        D.optionalHeaderValueByType[Origin](()) {
+            // pekko.http.scaladsl.model.headers.Origin
+            case Some(originHeader: Origin) =>
+                val originUrl: String = originHeader.value
+
+                if(allowedOrigins.contains(originUrl)) {
+                    D.options {
+
+                    }
+                }
+        }
+    }
 }
--- docs/backend_design.md
+++ docs/backend_design.md
@@ -1,9 +1,42 @@
 # backend
 
+## abstract
+
 This project has the "data-driven" naming rule.
 
 - create application.conf to connect to DB and Redis
 - create Main.scala and main/BUILD.bazel
+
+## coding rules
+
+As much as possible, dependencies and types should be explicitly written
+BUT never use alias for Type definition for consistancy and easy-to-read
+and, files which generally formatted like model files, can omit them.
+
+Model: can omit explicit dependency
+Repo:  can omit explicit dependency
+Routes: can omit explicit dependency
+Middleware(Handlers): never omit
+Service: never omit
+Main: never omit
+
+### examples
+
+```
+import org.apache.pekko.http.scaladsl.server.{Directives => D}._
+import org.apache.pekko.http.scaladsl.server.{Directive0 => D0, Route => R}
+
+    private def corsResponseHeaders(originUrl: String): Directive0 = { // never abbreviate for definition 
+        D.respondWithHeaders( // use prefix 
+            `Access-Control-Allow-Origin`(HttpOrigin(originUrl)),
+            `Access-Control-Allow-Methods`(allowedMethods),
+            `Access-Control-Allow-Headers`(allowedHeaders),
+            `Access-Control-Allow-Credentials`(true)
+        )
+    }
+```
+
+## structure
 
 backend/
 -- api/v1/
@@ -39,10 +72,10 @@ backend/
     -- workspaces
 -- main
 
-# Lib 
+## Lib 
 
-## JSON Library: Play JSON
+### JSON Library: Play JSON
 
-### Reason for adoption
+#### Reason for adoption
 
 - Spray JSON doesn't support more than 22 columns
