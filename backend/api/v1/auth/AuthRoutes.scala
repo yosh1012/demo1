c2ab99cba9bffb4b3086a96c0d9bd99c8885ccc0
yoshi1012 <carrotpaste1012@gmail.com>
2026-01-18 14:35:21 -0800

    feat: implemented auth api

--- backend/api/v1/auth/AuthRoutes.scala
+++ backend/api/v1/auth/AuthRoutes.scala
@@ -1,40 +1,66 @@
 package com.taskmanagement.api.v1.auth.demo1
 
 // dependencies
-import org.apache.pekko.http.scaladsl.server.Directives
+import org.apache.pekko.http.scaladsl.server.{Directives => D}
 import org.apache.pekko.http.scaladsl.server.Route
+import org.apache.pekko.http.scaladsl.model.StatusCodes
+import com.github.pjfanning.pekkohttpplayjson.PlayJsonSupport
+import scala.util.{Success, Failure}
+import com.taskmanagement.lib.auth.demo1.{SignupRequest, LoginRequest, TokenResponse}
 
-class AuthRoutes(authService: AuthService) {
-    val routes: Route = pathPrefix("auth") {
-        concat(
-            path("signup") {
-                post {
-                    complete("sign up a new user")
+class AuthRoutes(authService: AuthService) extends PlayJsonSupport {
+    val routes: Route = D.pathPrefix("auth") {
+        D.concat(
+            D.path("signup") {
+                D.post {
+                    D.entity(D.as[SignupRequest]) {(signupRequestData: SignupRequest) =>
+                        D.onComplete(authService.signup(signupRequestData)) {
+                            case Success(tokenResponseData: TokenResponse) =>
+                                D.complete(StatusCodes.Created, tokenResponseData) // 201
+
+                            case Failure(ex: IllegalArgumentException) =>
+                                D.complete(StatusCodes.BadRequest, ex.getMessage) // 400
+                            
+                            case Failure(ex: Throwable) =>
+                                D.complete(StatusCodes.InternalServerError, "Internal server error") // 500
+                        }
+                    }
                 }
             },
-            path("password") {
-                post {
-                    complete("reset user's password")
+            D.path("password") {
+                D.post {
+                    // TODO
+                    }
                 }
             },
-            path("login") {
-                post {
-                    complete("login user")
+            D.path("login") {
+                D.post {
+                    D.onComplete(D.as[LoginRequest]) { (loginRequestData: LoginRequest) =>
+                        case Success(tokenResponseData: TokenResponse) =>
+                            D.complete(StatusCodes.OK, tokenResponseData) // 200
+                        
+                        case Failure(ex: IllegalArgumentException) =>
+                            D.complete(StatusCodes.Unauthorized, ex.getMessage) // 401
+
+                        case Failure(ex: Throwable) ->
+                            D.complete(StatusCodes.InternalServerError, "Internal server error") // 500
                 }
             },
-            path("verify") {
-                get {
-                    complete("verify user's access token")
+            D.path("verify") {
+                D.get {
+                    AuthHandler.authenticate { (usr_id: Long) =>
+                        D.complete(StatusCodes.OK, s"{usr_id: $usr_id}") //200
+                    }
                 }
             },
-            path("refresh") {
-                post {
-                    complete("refresh user's access token")
+            D.path("refresh") {
+                D.post {
+                    D.complete("refresh user's access token") // TODO
                 }
             },
-            path("logout") {
-                post {
-                    complete("logout user")
+            D.path("logout") {
+                D.post {
+                    D.complete(StatusCodes.OK, "Logged out")
                 }
             }
         )
